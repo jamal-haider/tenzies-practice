91e6e710e4bbe9abc149f79b5cadd18d9e1af077
Jamal <jamal.hyderr@gmail.com>
2023-06-10 16:14:43 +0500

    tenzies won with confetti package

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Die from "./Die"
 import { nanoid } from "nanoid"
+import Confetti from 'react-confetti'
 
 function App() {
   const [dice, setDice] = useState(allNewDice())
+  const [tenzies, setTenzies] = useState(false)
   function generateNewDie(){
     return {
       id: nanoid(),
@@ -21,16 +23,46 @@ function App() {
     return diceArray
   }
 
-  const diceElement = dice.map(die => <Die key={die.id} data={die} />)
+  function toggleDice(id){
+    setDice(prevDice => prevDice.map(die => {
+      return die.id === id ? { ...die, isHeld: !die.isHeld } : die
+    }))
+  }
+
+  function rollDice(){
+    if(tenzies){
+      setTenzies(false)
+      setDice(allNewDice())
+    }
+    else{
+      setDice(prevDice => prevDice.map(die => {
+        return die.isHeld ? die : generateNewDie()
+      }))
+    } 
+  }
+
+  useEffect(() => {
+    const allHeld = dice.every(die => die.isHeld)
+    const firstDie = dice[0].value
+    const allSame = dice.every(die => die.value === firstDie)
+    if(allHeld && allSame){
+      setTenzies(true)
+      console.log("You Won!")
+    }
+  }, [dice])
+  
+
+  const diceElement = dice.map(die => <Die key={die.id} data={die} toggleDice={() => toggleDice(die.id)} />)
 
   return (
     <div className='container'>
+      {tenzies && <Confetti />}
       <h1 className='tenzies-heading'>Tenzies</h1>
       <p className='tenzies-description'>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className="dice-list">
         {diceElement}
       </div>
-      <button className='tenzies-btn'>Roll</button>
+      <button className='tenzies-btn' onClick={rollDice}>{tenzies ? "New Game" : "Roll"}</button>
     </div>
   )
 }
--- src/Die.jsx
+++ src/Die.jsx
@@ -1,6 +1,10 @@
 export default function Die(props){
+
+    const styles = {
+        backgroundColor: props.data.isHeld ? "#59E391" : "white"
+    }
     return(
-        <div className="die-face">
+        <div className="die-face" style={styles} onClick={props.toggleDice}>
             <h2>{props.data.value}</h2>
         </div>
     )
